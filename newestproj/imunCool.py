f688c61f7a5541ea034eff1084c679ebb56f3975
AnhPhuN <90109952+AnhPhuN@users.noreply.github.com>
2023-04-22 15:51:19 -0400

    save commit

--- newestproj/imunCool.py
+++ newestproj/imunCool.py
@@ -28,8 +28,70 @@ def DenseCaptioning(imgPath):
                                                             VisualFeatureTypes.image_type,
                                                             VisualFeatureTypes.objects,
                                                             VisualFeatureTypes.tags])
-    return result
+    tags = result.tags
+    adult = result.adult
+    brands = result.brands
+    categories = result.categories
+    color = result.color
+    description = result.description
+    faces = result.faces
+    image_type = result.image_type
+    objects = result.objects
+    readable_res = "Image Tags:\n"
+    for tag in tags:
+        readable_res += f"- {tag.name} (confidence: {tag.confidence})\n"
+    
+    readable_res += "\nAdult Content:\n"
+    readable_res += f"- Is Adult Content? {adult.is_adult_content}\n"
+    readable_res += f"- Adult Score: {adult.adult_score}\n"
+    readable_res += f"- Is Racy Content? {adult.is_racy_content}\n"
+    readable_res += f"- Racy Score: {adult.racy_score}\n"
+
+    if len(brands) > 0:
+        readable_res += "\nBrands:\n"
+        for brand in brands:
+            readable_res += f"- {brand.name} (confidence: {brand.confidence})\n"
+    
+    readable_res += "\nCategories:\n"
+    for category in categories:
+        readable_res += f"- {category.name} (confidence: {category.score})\n"
+
+    readable_res += "\nDominant Color:\n"
+    readable_res += f"- Background Color: {color.dominant_color_background}\n"
+    readable_res += f"- Foreground Color: {color.dominant_color_foreground}\n"
+    readable_res += f"- Accent Color: {color.accent_color}\n"
+    
+    if len(description.captions) > 0:
+        readable_res += "\nImage Description:\n"
+        for caption in description.captions:
+            readable_res += f"- {caption.text} (confidence: {caption.confidence})\n"
+    
+    if len(faces) > 0:
+        readable_res += "\nFaces:\n"
+        for face in faces:
+            readable_res += f"- Gender: {face.gender}\n"
+            readable_res += f"- Age: {face.age}\n"
+            readable_res += f"- Emotion: {face.emotion}\n"
+    
+    if image_type.clip_art_type != 0:
+        readable_res += "- Clip Art Detected\n"
+    elif image_type.line_drawing_type != 0:
+        readable_res += "- Line Drawing Detected\n"
+    
+    if len(objects) > 0:
+        readable_res += "\nObjects:\n"
+        for obj in objects:
+            readable_res += f"- {obj.object_property} (confidence: {obj.confidence})\n"
+    
+    return(readable_res)
     # do something
+    '''
+    Authenticate
+    Authenticates your credentials and creates a client.
+    '''
+    subscription_key = os.environ["subscriptionKeyAzure"]
+
+    computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))    # do something
     '''
     Authenticate
     Authenticates your credentials and creates a client.
